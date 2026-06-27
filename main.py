4d5e3c52074bd1006b34ffabb026dc471fc8ed20
SGM24 <beckers.rival@gmail.com>
2026-06-27 12:30:31 -0400

    Revert "idk how to undo current code so im undoing this"
    This reverts commit 26933820893999c7071d4b1867fff37046e5b9dc.

--- main.py
+++ main.py
@@ -125,12 +125,8 @@ def handle_IO():
     # Main thread handles graphics ticks
     timer = QtCore.QTimer()
     timer.timeout.connect(update_gui_and_consume_queue)
-    frame_ms = (
-        oscilloscope.Channel.buffer_size
-        / oscilloscope.settings["rx_rate"]
-    ) * 1000
-
-    timer.start(int(frame_ms))
+    fps = 60
+    timer.start(int(1000 / fps))
     
     interrupt_timer = QtCore.QTimer()
     interrupt_timer.start(100)
